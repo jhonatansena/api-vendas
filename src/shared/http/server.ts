850334e8d7a162c9f1a940a14223c2f1433ceaa5
Jhonatan Sena <jcostasenna@hotmail.com>
2021-06-27 21:31:41 -0300

    feature:tratando erros no server da aplicaçao

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,7 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
+import { AppError } from '@shared/errors/AppError';
 
 
 const app = express();
@@ -9,6 +10,20 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use((error: Error, request: Request, response: Response, next: NextFunction) =>{
+  if(error instanceof AppError){
+    return response.json({
+      status: 'error',
+      message: error.message
+    });
+  }
+  return response.status(500).json({
+      status: 'error',
+      message: `Internal server error - ${error.message}`
+
+  })
+})
 
 app.listen(3000, () => {
 console.log("Server running on port 3000!")
