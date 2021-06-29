e4b58af4bdb7f14dcca2c857e2476b69b7d7a425
Jhonatan Sena <jcostasenna@hotmail.com>
2021-06-29 18:21:52 -0300

    feature: criada service para listar products

---
+++ src/modules/products/services/ListProductService.ts
@@ -0,0 +1,17 @@
+import { getCustomRepository } from 'typeorm';
+import { ProductsRepositories } from '../typeorm/repositories/ProductsRepositories';
+
+
+class ListProductService{
+
+  async execute(){
+    const productRepositories = getCustomRepository(ProductsRepositories);
+
+    const products = await productRepositories.find();
+
+    return products;
+  }
+
+}
+
+export {ListProductService}
