085fd1b34dbea2d4a9fe5f00461b9aa1f9ca9819
Jhonatan Sena <jcostasenna@hotmail.com>
2021-06-29 09:37:45 -0300

    feature:Criado repository de product

---
+++ src/modules/products/typeorm/repositories/ProductsRepositories.ts
@@ -0,0 +1,14 @@
+import {Repository, EntityRepository} from "typeorm";
+import { Product } from "../entities/Product";
+
+@EntityRepository(Product)
+class ProductsRepositories extends Repository<Product>{
+public async findByName(name: string): Promise<Product | undefined>{
+
+  const product = this.findOne(name);
+
+return product;
+}
+}
+
+export {ProductsRepositories}
