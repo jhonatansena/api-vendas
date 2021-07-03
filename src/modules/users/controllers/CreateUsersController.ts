4d06af4a8642e08d36d13169f8de8071a0381f11
Jhonatan Sena <jcostasenna@hotmail.com>
2021-07-03 20:49:52 -0300

    feat: controller para criar usuario

---
+++ src/modules/users/controllers/CreateUsersController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+import { CreateUserService } from '@modules/users/services/CreateUserService';
+
+class CreateUsersController{
+  async create(request: Request, response: Response){
+    const {name, email, password, avatar} = request.body;
+
+    const createUserService = new CreateUserService();
+    const user = await createUserService.execute({name,
+                                                  email,
+                                                  password,
+                                                   avatar});
+
+    return response.json(user)
+
+  }
+}
+export {CreateUsersController}
