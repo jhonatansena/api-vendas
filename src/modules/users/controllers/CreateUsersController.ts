0ce891154fe3b9a9121064c68b07db33a5de9c4e
Jhonatan Sena <jcostasenna@hotmail.com>
2021-07-07 19:51:06 -0300

    feat: instalação do jest

--- src/modules/users/controllers/CreateUsersController.ts
+++ src/modules/users/controllers/CreateUsersController.ts
@@ -1,18 +1,20 @@
 import { Request, Response } from 'express';
 import { CreateUserService } from '@modules/users/services/CreateUserService';
 
-class CreateUsersController{
-  async create(request: Request, response: Response){
-    const {name, email, password, avatar} = request.body;
+class CreateUsersController {
+  async create(request: Request, response: Response) {
+    const { name, email, password, avatar } = request.body;
 
     const createUserService = new CreateUserService();
-    const user = await createUserService.execute({name,
-                                                  email,
-                                                  password,
-                                                   avatar});
+    const user = await createUserService.execute(
+    {
+      name,
+      email,
+      password,
+      avatar,
+    });
 
-    return response.json(user)
-
+    return response.json(user);
   }
 }
-export {CreateUsersController}
+export { CreateUsersController };
