890808eada25e279141e665d089ca377fa1f2ff2
Jhonatan Sena <jcostasenna@hotmail.com>
2021-07-16 20:45:49 -0300

    feat: controller to reset password

---
+++ src/modules/users/controllers/ResetPasswordController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import { ResetPasswordService } from '../services/ResetPasswordService';
+
+class ResetPasswordController {
+  async create(request: Request, response: Response): Promise<Response> {
+    const { password, token } = request.body;
+
+    const resetPasswordService = new ResetPasswordService();
+    await resetPasswordService.execute({
+      password,
+      token,
+    });
+
+    return response.status(204).json();
+  }
+}
+export { ResetPasswordController };
