abdbb4ceb2542752ae95703601d35c8e73e1935d
Jhonatan Sena <jcostasenna@hotmail.com>
2021-07-16 20:46:44 -0300

    fix: add Promise

--- src/modules/users/controllers/ForgotPasswordController.ts
+++ src/modules/users/controllers/ForgotPasswordController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import { SendForgotPasswordEmailService } from '../services/SendForgotPasswordEmailService';
 
 class ForgotPasswordController {
-  async create(request: Request, response: Response) {
+  async create(request: Request, response: Response): Promise<Response> {
     const { email } = request.body;
 
     const sendForgotPasswordEmail = new SendForgotPasswordEmailService();
