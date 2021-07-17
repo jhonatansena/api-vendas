66a8cf37325dbdc4e944469d111a49d5688b6950
Jhonatan Sena <jcostasenna@hotmail.com>
2021-07-16 21:12:07 -0300

    feat: rota para resetar password

--- src/modules/users/routes/password.routes.ts
+++ src/modules/users/routes/password.routes.ts
@@ -2,8 +2,10 @@ import { celebrate, Segments } from 'celebrate';
 import { Router } from 'express';
 import Joi from 'joi';
 import { ForgotPasswordController } from '../controllers/ForgotPasswordController';
+import { ResetPasswordController } from '../controllers/ResetPasswordController';
 
 const forgotPasswordController = new ForgotPasswordController();
+const resetPasswordController = new ResetPasswordController();
 
 const passwordRouter = Router();
 
@@ -15,6 +17,18 @@ passwordRouter.post(
     },
   }),
   forgotPasswordController.create,
+);
+
+passwordRouter.post(
+  '/reset',
+  celebrate({
+    [Segments.BODY]: {
+      token: Joi.string().uuid().required(),
+      password: Joi.string().required(),
+      password_confirmation: Joi.string().required().valid(Joi.ref('password')),
+    },
+  }),
+  resetPasswordController.create,
 );
 
 export { passwordRouter };
