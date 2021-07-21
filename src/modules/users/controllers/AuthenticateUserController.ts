a93e3e49aeacab80c5f992b9a0265ff9fb26a309
Jhonatan Sena <jcostasenna@hotmail.com>
2021-07-21 20:11:19 -0300

    fix: ajuste de lint

--- src/modules/users/controllers/AuthenticateUserController.ts
+++ src/modules/users/controllers/AuthenticateUserController.ts
@@ -1,18 +1,15 @@
 import { Request, Response } from 'express';
 import { AuthenticateUserService } from '../services/AuthenticateUserService';
 
-
-class AuthenticateUserController{
-  async authenticate(request: Request, response: Response){
-    const {email, password} = request.body;
+class AuthenticateUserController {
+  async authenticate(request: Request, response: Response): Promise<Response> {
+    const { email, password } = request.body;
 
     const authenticateUserService = new AuthenticateUserService();
-    const user = await authenticateUserService.execute({email, password});
+    const user = await authenticateUserService.execute({ email, password });
 
     return response.json(user);
-
   }
-
 }
 
-export {AuthenticateUserController}
+export { AuthenticateUserController };
