50d6a2de036e39398d4080430f73ab7500cae74a
Jhonatan Sena <jcostasenna@hotmail.com>
2021-07-21 20:14:55 -0300

    fix: ajustando o campo avatar

--- src/modules/users/controllers/CreateUsersController.ts
+++ src/modules/users/controllers/CreateUsersController.ts
@@ -2,16 +2,14 @@ import { Request, Response } from 'express';
 import { CreateUserService } from '@modules/users/services/CreateUserService';
 
 class CreateUsersController {
-  async create(request: Request, response: Response) {
-    const { name, email, password, avatar } = request.body;
+  async create(request: Request, response: Response): Promise<Response> {
+    const { name, email, password } = request.body;
 
     const createUserService = new CreateUserService();
-    const user = await createUserService.execute(
-    {
+    const user = await createUserService.execute({
       name,
       email,
       password,
-      avatar,
     });
 
     return response.json(user);
