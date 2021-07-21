cab51bb2bcef7283ff9f5648b3d474e0a88c7c01
Jhonatan Sena <jcostasenna@hotmail.com>
2021-07-21 19:32:09 -0300

    fix: fixing promise from user

--- src/modules/users/services/ListUserService.ts
+++ src/modules/users/services/ListUserService.ts
@@ -1,8 +1,9 @@
 import { getCustomRepository } from 'typeorm';
 import { UsersRepositories } from '@modules/users/typeorm/repositories/UsersRepositories';
+import { User } from '../typeorm/entities/User';
 
 class ListUserService {
-  async execute() {
+  async execute(): Promise<User[]> {
     const userRepositories = getCustomRepository(UsersRepositories);
 
     const user = await userRepositories.find();
