f1352cccc25ade7c9806b52ad6065f756ac20dec
Jhonatan Sena <jcostasenna@hotmail.com>
2021-07-23 15:15:41 -0300

    feature: add update customer service

---
+++ src/modules/customers/services/UpdateCustomerService.ts
@@ -0,0 +1,36 @@
+import { getCustomRepository } from 'typeorm';
+import { AppError } from '@shared/errors/AppError';
+import { Customer } from '../typeorm/entities/Customer';
+import { CustomersRepositories } from '../typeorm/repositories/CustomersRepositories';
+
+interface IRequest {
+  id: string;
+  name: string;
+  email: string;
+}
+
+class UpdateProfileService {
+  async execute({ id, name, email }: IRequest): Promise<Customer> {
+    const customersRepositories = getCustomRepository(CustomersRepositories);
+
+    const customer = await customersRepositories.findById(id);
+
+    if (!customer) {
+      throw new AppError('Customer not found');
+    }
+
+    const customerExists = await customersRepositories.findByEmail(email);
+
+    if (customerExists && customer.email !== customer.email) {
+      throw new AppError('There is already one user with this email.');
+    }
+
+    customer.name = name;
+    customer.email = email;
+
+    await customersRepositories.save(customer);
+    return customer;
+  }
+}
+
+export { UpdateProfileService };
