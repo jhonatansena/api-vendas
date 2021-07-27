69a937c6e419a65a5bd9275fa05be406074be9ef
Jhonatan Sena <jcostasenna@hotmail.com>
2021-07-27 12:30:29 -0300

    feature: add entity OrdersProducts

---
+++ src/modules/orders/typeorm/entities/OrdersProducts.ts
@@ -0,0 +1,47 @@
+import { Customer } from '@modules/customers/typeorm/entities/Customer';
+import { Product } from '@modules/products/typeorm/entities/Product';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { v4 as uuid } from 'uuid';
+import { Order } from './Order';
+
+@Entity('orders_products')
+class OrderProducts {
+  @PrimaryColumn('uuid')
+  id: string;
+
+  @ManyToOne(() => Order, order => order.order_products)
+  @JoinColumn({ name: 'order_id' })
+  order: Order;
+
+  @ManyToOne(() => Product, product => product.order_products)
+  @JoinColumn({ name: 'product_id' })
+  product: Product;
+
+  @Column('decimal')
+  price: number;
+
+  @Column('int')
+  quantity: number;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
+
+export { OrderProducts };
