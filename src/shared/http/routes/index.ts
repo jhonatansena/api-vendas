08f4c89879ffb8fb7959bfd269eb6654930d402c
Jhonatan Sena <jcostasenna@hotmail.com>
2021-08-12 15:31:58 -0300

    feature: index route from orders

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -6,6 +6,7 @@ import { authenticateRouter } from '@modules/users/routes/authenticate.routes';
 import { passwordRouter } from '@modules/users/routes/password.routes';
 import { profileRouter } from '@modules/users/routes/profile.routes';
 import { customerRouter } from '@modules/customers/routes/customerts.routes';
+import { ordersRouter } from '@modules/orders/routes/orders.routes';
 
 const routes = Router();
 
@@ -15,5 +16,6 @@ routes.use('/authenticate', authenticateRouter);
 routes.use('/password', passwordRouter);
 routes.use('/profile', profileRouter);
 routes.use('/customers', customerRouter);
+routes.use('/orders', ordersRouter);
 
 export default routes;
