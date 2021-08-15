d79bf7cda0a2e667c4d74538b9b96e9daad914d2
Jhonatan Sena <jcostasenna@hotmail.com>
2021-08-15 12:03:46 -0300

    feature: Add pagination on Customer

--- src/modules/customers/services/ListCustomerService.ts
+++ src/modules/customers/services/ListCustomerService.ts
@@ -2,13 +2,25 @@ import { getCustomRepository } from 'typeorm';
 import { Customer } from '../typeorm/entities/Customer';
 import { CustomersRepositories } from '../typeorm/repositories/CustomersRepositories';
 
+interface IPaginateCustomer {
+  from: number;
+  to: number;
+  per_page: number;
+  total: number;
+  current_page: number;
+  prev_page: number | null;
+  next_page: number | null;
+  data: Customer[];
+}
+
 class ListCustomerService {
-  async execute(): Promise<Customer[]> {
+  async execute(): Promise<IPaginateCustomer> {
     const customersRepositories = getCustomRepository(CustomersRepositories);
+    const customers = await customersRepositories
+      .createQueryBuilder()
+      .paginate();
 
-    const customers = await customersRepositories.find();
-
-    return customers;
+    return customers as IPaginateCustomer;
   }
 }
 
